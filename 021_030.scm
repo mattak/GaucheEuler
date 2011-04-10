16e76c6e5448b10ef148c8a5c1cc8b0b288d8b23
Takuma Maruyama <maruyama.ylab@gmail.com>
2011-04-10 16:30:21 +0900

    p23

--- 021_030.scm
+++ 021_030.scm
@@ -32,3 +32,42 @@
          (sums 0))
     (apply + (map * dict (... 1 (length dict))))))
 
+
+;; problem 23. abundant number
+;;--------------------------------
+
+(define ans23
+  (let* ((abundants
+          (collection-if (lambda (x) (if (> (amicable x) x) #t #f)) (... 1 28123)))
+         (lst abundants)
+         (slst abundants)
+         (tmp 1)
+         (nums '())
+         (finded #f)
+         (continue1 #t)
+         (continue2 #t))
+    (dolist (a (... 1 28123))
+            (set! lst abundants)
+            (set! tmp (- a (car lst)))
+            (set! continue1 #t)
+            (set! finded #f)
+            (while (and continue1 (not (null? lst)))
+                   (set! slst abundants)
+                   (set! tmp (- a (car lst)))
+                   (if (> tmp 0)
+                       (set! continue2 #t)
+                       (set! continue2 #f))
+                   (while (and continue2 (not (null? slst)))
+                          (cond ((= (car slst) tmp)
+                                 (set! continue1 #f)
+                                 (set! continue2 #f)
+                                 (set! finded #t))
+                                ((> (car slst) tmp)
+                                 (set! continue2 #f))
+                                (#t #f))
+                          (set! slst (cdr slst)))
+                   (set! lst (cdr lst)))
+            (if (not finded) (push! nums a)))
+    (apply + nums)))
+
+
